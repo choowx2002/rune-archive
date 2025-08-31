0a7b562caa27eeca8eb18872bc74573605fbafde
wx <choowx2002@gmail.com>
2025-08-31 12:29:02 +0800

    update nothing

--- public/js/build/layouts/setup-cursor.js
+++ public/js/build/layouts/setup-cursor.js
@@ -1,1 +1,5 @@
-new cursoreffects.emojiCursor({emoji:["❄️"],length:4,size:8});
+new cursoreffects.emojiCursor({ 
+  emoji: ["❄️"] ,
+  length: 4,
+  size: 8
+});
--- source/battlefield/index.md
+++
@@ -1,5 +0,0 @@
----
-title: battlefield
-date: 2025-08-27 09:44:55
-layout: battlefield
----
